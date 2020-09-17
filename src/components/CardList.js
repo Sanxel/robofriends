496bcef13948eb7d9ef466422628cfd4a0d6fdd6
Alex Savenko <alexx7187@gmail.com>
2020-09-17 10:23:40 +0300

    First commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,22 +2,23 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
-}
+	// if (true) {
+	// 	throw new Error('Nooo');
+	// }
+	return (
+		<div>
+			{robots.map((user, i) => {
+				return (
+					<Card
+						key={i}
+						id={robots[i].id}
+						name={robots[i].name}
+						email={robots[i].email}
+					/>
+				);
+			})}
+		</div>
+	);
+};
 
 export default CardList;
